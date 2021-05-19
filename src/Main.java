4a6d193064a43a7c12ee904c41435f7fd1984de0
이진석 <wlstjrwlstjr18@gmail.com>
2021-05-19 20:15:42 +0900

    dynamic programming

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,7 @@
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Main{
 
-
-
-    public static void main(String[] args){
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        int n=kb.nextInt();
-        int[] a=new int[n];
-        for(int i=0; i<n; i++){
-            a[i]=kb.nextInt();
-        }
-        int m=kb.nextInt();
-        int[] b=new int[m];
-        for(int i=0; i<m; i++){
-            b[i]=kb.nextInt();
-        }
-        for(int x : T.solution(n, m, a, b)) System.out.print(x+" ");
-    }
-
-    private ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
-        ArrayList<Integer> answer = new ArrayList<Integer>();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        int ia = 0;
-        int ib = 0;
-        while(ia <n && ib < m){
-            if(a[ia] == b[ib]){
-                answer.add(a[ia]);
-                ia++;
-                ib++;
-            }else if(a[ia] > b[ib]){
-                ib++;
-            }else{
-                ia++;
-            }
-
-        }
-
-
-
-
-
-        return answer;
-    }
 }
 
 
