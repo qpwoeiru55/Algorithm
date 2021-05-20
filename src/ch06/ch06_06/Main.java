2e02f2e52ffe0ccf64e60bc3a4bb571241015cd1
이진석 <wlstjrwlstjr18@gmail.com>
2021-05-20 16:47:06 +0900

    -

--- src/ch06/ch06_06/Main.java
+++ src/ch06/ch06_06/Main.java
@@ -11,8 +11,12 @@ public class Main {
         Scanner kb = new Scanner(System.in);
         int n=kb.nextInt();
         int[] arr=new int[n];
-        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
-        for(int x : T.solution(n, arr)) System.out.print(x+" ");
+        for(int i=0; i<n; i++){
+            arr[i]=kb.nextInt();
+        }
+        for(int x : T.solution(n, arr)){
+            System.out.print(x+" ");
+        }
     }
 
     public ArrayList<Integer> solution(int n, int[] arr){
--- src/ch06/ch06_09/Main.java
+++ src/ch06/ch06_09/Main.java
@@ -1,39 +1,7 @@
 package ch06.ch06_09;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.stream.IntStream;
-
-public class Main {
-    public static void main(String[] args){
-        Main T = new Main();
-        Scanner kb = new Scanner(System.in);
-        int n=kb.nextInt();
-        int m=kb.nextInt();
-        int[] arr=new int[n];
-        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
-        System.out.println(T.solution(n, m, arr));
-    }
-
-    private int solution(int n, int m, int[] arr) {
-        int answer = 0;
-        int lt = Arrays.stream(arr).max().getAsInt();
-        int rt = Arrays.stream(arr).sum();
-        while(lt<=rt){
-            int mid = (lt + rt) / 2;
-            if(count(arr, mid)<=m){
-                answer = mid;
-                rt = mid -1;
-            }else{
-                lt = mid + 1;
-            }
-        }
-
-        answer = lt;
-        return answer;
-    }
-
+import java.util.*;
+class Main {
     public int count(int[] arr, int capacity){
         int cnt=1, sum=0;
         for(int x : arr){
@@ -46,5 +14,33 @@ public class Main {
             }
         }
         return cnt;
+    }
+
+    public int solution(int n, int m, int[] arr){
+        int answer=0;
+        int lt=Arrays.stream(arr).max().getAsInt();
+        int rt=Arrays.stream(arr).sum();
+        while(lt<=rt){
+            int mid=(lt+rt)/2;
+            if(count(arr, mid)<=m){
+                answer=mid;
+                rt=mid-1;
+            }
+            else{
+                lt=mid+1;
+            }
+        }
+        return answer;
+    }
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int m=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++){
+            arr[i]=kb.nextInt();
+        }
+        System.out.println(T.solution(n, m, arr));
     }
 }
