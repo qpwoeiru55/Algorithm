90b2afe4c59388d239396de334ee0d67588ce2db
DESKTOP-1EDMQ3M\82104 <wlstjrwlstjr18@gmail.com>
2025-06-21 17:45:58 +0900

    문자열 압축/2진법 변환

---
+++ src/String/day12/Main.java
@@ -0,0 +1,23 @@
+package String.day12;
+
+import java.util.Scanner;
+
+class Main {
+    public String solution(int n, String s) {
+        String answer = "";
+        for(int i=0; i<n; i++){
+            String tmp = s.substring(i*7,i*7+7).replace('#', '1').replace('*', '0');
+            int num = Integer.parseInt(tmp, 2);
+            answer+=(char)num;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String str = kb.next();
+        System.out.println(T.solution(n, str));
+    }
+}
