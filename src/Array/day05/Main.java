f5b2f7a46c1edb87598e6e6d25f820832443a7b2
DESKTOP-1EDMQ3M\82104 <wlstjrwlstjr18@gmail.com>
2025-06-25 14:14:59 +0900

    에라토스테네스 / 소수 / 등수

---
+++ src/Array/day05/Main.java
@@ -0,0 +1,30 @@
+package Array.day05;
+
+import java.util.Scanner;
+
+public class Main {
+    public int solution(int num){
+        int answer = 0;
+        int[] arr = new int[num+1];
+        for(int i=2; i<=num; i++){
+            if(arr[i] == 0){
+                answer++;
+                for(int j=i; j<=num; j=j+i){
+                    if(arr[j]%i ==0){
+                        arr[j] = 1;
+                    }
+                }
+            }
+
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int num = kb.nextInt();
+        System.out.println(T.solution(num));
+    }
+}
