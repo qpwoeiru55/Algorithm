724a0b83b0062b7037e5de4190e085f57bd68b91
DESKTOP-1EDMQ3M\82104 <wlstjrwlstjr18@gmail.com>
2025-07-03 20:56:06 +0900

    HashMap / stack

---
+++ src/stackQue/day02/Main.java
@@ -0,0 +1,33 @@
+package stackQue.day02;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(T.solution(str));
+    }
+
+    private String solution(String str) {
+        String answer = "";
+        Stack<Character> characterStack = new Stack<Character>();
+        for(char x : str.toCharArray()){
+            if(x == '('){
+                characterStack.push(x);
+            }else if (Character.isAlphabetic(x)) {
+                if(!characterStack.isEmpty()){
+                    continue;
+                }else{
+                    answer += x;
+                }
+            }else if (x == ')') {
+                characterStack.pop();
+            }
+        }
+        return answer;
+    }
+}
