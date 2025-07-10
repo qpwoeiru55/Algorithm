e7afc147bb501a1f55d9d9960a114f29399b7f58
DESKTOP-1EDMQ3M\82104 <wlstjrwlstjr18@gmail.com>
2025-07-10 18:34:20 +0900

    결정 알고리즘

---
+++ src/SortAndSearch/day09/Main.java
@@ -0,0 +1,49 @@
+package SortAndSearch.day09;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int m=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+        System.out.println(T.solution(n, m, arr));
+    }
+
+    private int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        int lt = Arrays.stream(arr).max().getAsInt();
+        int rt = Arrays.stream(arr).sum();
+        while(lt<=rt){
+            int mid = (lt+rt)/2;
+            if(count(mid,arr) <= m){
+                answer = mid;
+                rt = mid-1;
+            }else{
+                lt = mid +1 ;
+            }
+
+        }
+
+        return answer;
+    }
+
+    private int count(int mid, int[] arr) {
+        int cnt = 1;
+        int sum = 0;
+        for(int x : arr){
+            if(sum+x > mid){
+                cnt++;
+                sum=x;
+            }else{
+                sum+=x;
+            }
+        }
+        return cnt;
+    }
+}
---
+++ src/SortAndSearch/day10/Main.java
@@ -0,0 +1,46 @@
+package SortAndSearch.day10;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n=kb.nextInt();
+        int c=kb.nextInt();
+        int[] arr=new int[n];
+        for(int i=0; i<n; i++) arr[i]=kb.nextInt();
+        System.out.println(T.solution(n, c, arr));
+    }
+
+    private int solution(int n, int c, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 1;
+        int rt = arr[n-1];
+        while(lt<=rt){
+            int mid = (lt+rt)/2;
+            if(dis(arr,mid) >= c){
+                answer = mid;
+                lt = mid+1;
+            }else{
+                rt = mid-1;
+            }
+        }
+        return answer;
+    }
+
+    private int dis(int[] arr, int mid) {
+        int dis=arr[0];
+        int cnt=1;
+        for(int x : arr){
+            if(x-dis>=mid){
+                cnt++;
+                dis=x;
+            }
+        }
+        return  cnt;
+    }
+
+}
